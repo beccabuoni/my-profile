0884367ce0e8cdfb16da854028978ad41110fc02
beccabuoni <38893323+beccabuoni@users.noreply.github.com>
2018-09-17 09:35:26 -0400

    Update silly.sh

--- silly.sh
+++ silly.sh
@@ -2,6 +2,8 @@
 
 echo "This is a silly script" > /tmp/silly.txt
 
+sudo yum clean all
+sudo yum -y update
 sudo yum -y install httpd
 sudo firewall-cmd --permanent --add-port=80/tcp
 sudo firewall-cmd --permanent --add-port=443/tcp
